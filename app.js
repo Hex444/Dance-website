27aff1255c07cb017ae36954b6dc355824264e0e
realhex444 <ayaansanjeev@gmail.com>
2022-01-04 16:09:24 +0530

    designed the mission and introsection a bit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 8000;
+const port = 80;
 
 
 app.set('view engine', 'pug'); //set template engine as pug
